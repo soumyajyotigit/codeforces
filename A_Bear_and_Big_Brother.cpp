48d1a4d79dfc5ba14008715ea6319b2691f85f34
Soumyajyoti Karmakar <ksoumyajyoti@gmail.com>
2023-01-25 11:07:59 +0530

    Add files via upload

---
+++ A_Bear_and_Big_Brother.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c=0;
+    cin>>a>>b;
+    if(a>b){
+        c=0;
+    }
+    while(a<=b){
+        a=a*3;
+        b=b*2;
+        c++;
+    }
+    cout<<c<<endl;
+
+}
